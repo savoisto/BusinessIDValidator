74a8d9df8d41358920dce74edc05c7b0faeccf45
savoisto <isto.savolainen@hotmail.com>
2020-01-25 00:59:07 +0200

    initial commit

---
+++ BusinessIdSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessIdSpecification
+{
+    public class BusinessIdSpecification<TEntity> : ISpecification<TEntity> where TEntity : class, new()
+    {
+        public IEnumerable<string> ReasonsForDissatisfaction => throw new NotImplementedException();
+
+        static void Main()
+        {
+
+        }
+
+        public bool IsSatisfiedBy(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/ISpecification.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessIdSpecification
+{
+    public interface ISpecification<in TEntity>
+    {
+        IEnumerable<string> ReasonsForDissatisfaction { get; }
+
+        bool IsSatisfiedBy(TEntity entity);
+    }
+}
