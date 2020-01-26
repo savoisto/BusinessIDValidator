b47a9d182473d065c236b7b40266cd19d256aca2
savoisto <isto.savolainen@hotmail.com>
2020-01-26 21:12:14 +0200

    Basic regex matching of a string

--- BusinessIdSpecification.cs
+++ BusinessIdSpecification.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace BusinessIdSpecification
 {
-    public class BusinessIdSpecification<TEntity> : ISpecification<TEntity> where TEntity : class, new()
+    public class BusinessIdSpecification : ISpecification<string>
     {
         public IEnumerable<string> ReasonsForDissatisfaction => throw new NotImplementedException();
 
@@ -12,9 +13,20 @@ namespace BusinessIdSpecification
 
         }
 
-        public bool IsSatisfiedBy(TEntity entity)
+        /// <summary>
+        /// Check if string is in business ID format. 7 Digits followed by a dash and a control mark. 
+        /// Link to YTJ, business information system specifications of a <a href="https://www.ytj.fi/en/index/businessid.html">Business ID</a>
+        /// </summary>
+        /// <param name="bId">String to check</param>
+        /// <returns></returns>
+        public bool IsSatisfiedBy(string bId)
         {
-            throw new NotImplementedException();
+            //Regex check
+            var match = Regex.Match(bId, @"^[\d]{7}-[\d]$");
+
+            //TODO: Reason for dissatisfaction
+
+            return match.Success;
         }
     }
 }
