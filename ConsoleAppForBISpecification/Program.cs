f2dbb427f3ba4142e704e47447f157f833b6f09e
savoisto <isto.savolainen@hotmail.com>
2020-01-30 01:11:30 +0200

    Executable project to use the dll

---
+++ ConsoleAppForBISpecification/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessIdSpecification;
+
+namespace ConsoleAppForBISpecification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Business ID to validate: ");
+            var bId = Console.ReadLine();
+            BusinessIdSpecification.BusinessIdSpecification bIdValidator = new BusinessIdSpecification.BusinessIdSpecification();
+            bool success = bIdValidator.IsSatisfiedBy(bId);
+
+            string validStatusString = "";
+            if (success)
+            {
+                validStatusString = "valid";
+            }
+            else
+            {
+                validStatusString = "not valid";
+            }
+
+
+            Console.WriteLine("Provided business ID was " + validStatusString);
+
+            if(!success)
+            {
+                using (IEnumerator<string> iterator = bIdValidator.ReasonsForDissatisfaction.GetEnumerator())
+                {
+                    Console.WriteLine("Reason for failure: ");
+                    while (iterator.MoveNext())
+                    {
+                        Console.WriteLine(iterator.Current.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
